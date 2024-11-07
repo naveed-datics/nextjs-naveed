82a2ac991c9a7ed8c53d78c4d74dfff4992d17ba
Naveed Ahmed <naveed.ahmed@datis.ai>
2024-11-07 20:41:38 +0500

    quick fix

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,9 +6,7 @@ export default async function ProductPage() {
       <ProductDetail
         image="https://flowbite.com/docs/images/blog/image-1.jpg"
         title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport"
-        rating={4}
         price={599}
-        link="/product/1"
       />
     </>
   );
